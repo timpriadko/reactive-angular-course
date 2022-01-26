838cf0167e8de074e8d17b55fec0a51ef3883509
timpriadko <timpriadko@gmail.com>
2022-01-26 18:11:22 +0200

    added courses store; refactored home page and course-dialog component according to the courses store;

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -8,6 +8,7 @@ import {throwError} from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 @Component({
     selector: 'course-dialog',
@@ -25,8 +26,7 @@ export class CourseDialogComponent implements AfterViewInit {
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService,
-        private loadingService: LoadingService,
+        private coursesStore: CoursesStore,
         private messagesService: MessagesService) {
 
         this.course = course;
@@ -46,24 +46,21 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
 
-      const changes = this.form.value;
+        const changes = this.form.value;
 
-      const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
-        .pipe(
-            catchError(err => {
-              const message = 'Could not save course';
-              console.log(message, err);
-              this.messagesService.showErrors(message);
-              return throwError(err);
-            })
-        );
+        this.coursesStore.saveCourse(this.course.id, changes)
+            .pipe(
+                catchError(err => {
+                    const message = 'Could not save course';
+                    console.log(message, err);
+                    this.messagesService.showErrors(message);
+                    return throwError(err);
+                })
+            )
+            .subscribe();
 
-      this.loadingService.showLoaderUntilCompleted(saveCourse$)
-        .subscribe(
-            val => {
-                this.dialogRef.close(val);
-            }
-        )
+
+        this.dialogRef.close(changes);
     }
 
     close() {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 
 @Component({
@@ -23,7 +24,7 @@ export class HomeComponent implements OnInit {
 
 
   constructor(
-    private coursesService: CoursesService,
+    private coursesStore: CoursesStore,
     private loadingService: LoadingService,
     private messagesService: MessagesService) {
 
@@ -35,28 +36,9 @@ export class HomeComponent implements OnInit {
 
   reloadCourses() {
 
-    const courses$  = this.coursesService.loadAllCourses()
-      .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo)),
-        catchError(err => {
-          const message = 'Could not load courses';
-          this.messagesService.showErrors(message);
-          console.log(message, err);
-          return throwError(err);
-        })
-      );
+    this.beginnerCourses$ = this.coursesStore.filterByCategory('BEGINNER');
 
-    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
-
-    this.beginnerCourses$ = loadCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == "BEGINNER"))
-      )
-
-    this.advancedCourses$ = loadCourses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == "ADVANCED"))
-      )
+    this.advancedCourses$ = this.coursesStore.filterByCategory('ADVANCED');
 
   }
 
